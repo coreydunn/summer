670f86975720223ffba6e5576b829eaf4936ccee
coreydunn <jumpingkanga@outlook.com>
2022-09-16 22:51:59 -0500

    multiple files

--- singlethread.c
+++ singlethread.c
@@ -11,32 +11,35 @@
 
 int main(int argc,char**argv)
 {
-	int fd=0;
 	SUM_TYPE hash=0;
 	char buf[BLOCK_SIZE];
-	size_t nr=0;
+	int fd=0;
 	size_t chars=0;
+	size_t nr=0;
 
-	if(argc>1)
-		fd=open(argv[1],O_RDONLY);
-	if(fd<0)
+	for(size_t fi=1;fi<argc;++fi)
 	{
-		write(2,"error: file\n",12);
-		exit(1);
+		if(argc>1)
+			fd=open(argv[fi],O_RDONLY);
+		if(fd<0)
+		{
+			write(2,"error: file\n",12);
+			exit(1);
+		}
+
+		while((nr=read(fd,buf,BLOCK_SIZE)))
+		{
+			for(size_t i=0;i<nr;++i)
+				hash+=buf[i];
+			chars+=nr;
+			//hash+=b;
+		}
+
+		//printf("total read: %lu\n",chars);
+		printf("%lu\t%s\n",hash,argv[fi]);
+		//write(1,&hash,sizeof(hash));
+
+		if(fd!=0)
+			close(fd);
 	}
-
-	while((nr=read(fd,buf,BLOCK_SIZE)))
-	{
-		for(size_t i=0;i<nr;++i)
-			hash+=buf[i];
-		chars+=nr;
-		//hash+=b;
-	}
-
-	//printf("total read: %lu\n",chars);
-	printf("%lu\t%s\n",hash,argv[1]);
-	//write(1,&hash,sizeof(hash));
-
-	if(fd!=0)
-		close(fd);
 }
